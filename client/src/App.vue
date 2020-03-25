de2571ba0abc9a885a30fe6e45594ded141c61b3
Armaan Lala <armaanlala@gmail.com>
2020-03-24 22:45:08 -0400

    Setting up vue

---
+++ client/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <Home/>
+  </div>
+</template>
+
+<script>
+import Home from './components/Home.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Home
+  }
+}
+</script>
+
+<style>
+</style>
