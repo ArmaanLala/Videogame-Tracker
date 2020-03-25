6a3161d2a215c7d72970183bc2cbeda91a6e693d
Armaan Lala <armaanlala@gmail.com>
2020-03-25 17:22:44 -0400

    API is set up and am creating templates for the UI

---
+++ client/src/router/router.js
@@ -0,0 +1,105 @@
+import Vue from "vue";
+import Router from "vue-router";
+import Home from "../components/Home";
+import NewEmployee from "../components/NewEmployee";
+import ViewEmployee from "../components/ViewEmployee";
+import EditEmployee from "../components/EditEmployee";
+import Login from "../components/Login";
+import Register from "../components/Register";
+import firebase from "firebase";
+
+Vue.use(Router);
+
+let router = new Router({
+    mode: "history",
+    routes: [
+        {
+            path: "/",
+            name: "home",
+            component: Home
+        },
+        {
+            path: "/login",
+            name: "login",
+            component: Login,
+            meta: {
+                requiresGuest: true
+            }
+        },
+        {
+            path: "/register",
+            name: "register",
+            component: Register,
+            meta: {
+                requiresGuest: true
+            }
+        },
+        {
+            path: "/new",
+            name: "new-employee",
+            component: NewEmployee,
+            meta: {
+                requiresAuth: true
+            }
+        },
+        {
+            path: "/edit/:employee_id",
+            name: "edit-employee",
+            component: EditEmployee,
+            meta: {
+                requiresAuth: true
+            }
+        },
+        {
+            path: "/:employee_id",
+            name: "view-employee",
+            component: ViewEmployee,
+            meta: {
+                requiresAuth: true
+            }
+        }
+    ]
+});
+
+//Nav Guards
+
+router.beforeEach((to, from, next) => {
+    //Check for required Auth guard
+    if (to.matched.some(record => record.meta.requiresAuth)) {
+        //Check if NOT Logged in
+        if (!firebase.auth().currentUser) {
+            // GO to login page
+            next({
+                path: '/login',
+                query: {
+                    redirect: to.fullPath
+                }
+            });
+        } else {
+            // Proceed to the route
+            next()
+        }
+    } else if (
+        to.matched.some(record => 
+            record.meta.requiresGuest
+        )
+    ) {
+        //Check if Logged in
+        if (firebase.auth().currentUser) {
+            // GO to login page
+            next({
+                path: '/',
+                query: {
+                    redirect: to.fullPath
+                }
+            });
+        } else {
+            // Proceed to the route
+            next()
+        }
+    } else {
+        next()
+    }
+});
+
+export default router;
--- server/routes/api/images.js
+++ server/routes/api/images.js
@@ -8,7 +8,8 @@ router.get('/:gameId', async(req,res) => {
     const id = req.params.gameId;
     const resp = await fetch(`https://api.thegamesdb.net/v1/Games/Images?apikey=TMP_TEST_KEY&games_id=${req.params.gameId}`)
     const data = await resp.json();
-    const url = data.data.base_url.original + data.data.images[id][0].filename
+    const url = `https://cdn.thegamesdb.net/images/original/boxart/front/${req.params.gameId}-1.jpg`
+    // const url = data.data.base_url.original + data.data.images[id][0].filename
     return res.status(200).json(url);
     }
     catch(err){
