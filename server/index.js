d5901345f7ac16dd73e54d57acee31606c0f7c96
armaanlala <armaanlala@gmail.com>
2020-04-27 18:08:38 -0400

    Fixed static index problem

--- server/index.js
+++ server/index.js
@@ -12,6 +12,13 @@ app.use('/api/images',images);
 const id = require('./routes/api/name');
 app.use('/api/name',id);
 
+if (process.env.NODE_ENV === 'production') {
+    // Static folder
+    app.use(express.static(__dirname + '/public/'));
+  
+    // Handle SPA
+    app.get(/.*/, (req, res) => res.sendFile(__dirname + '/public/index.html'));
+  }
 
 const port = process.env.PORT || 5000;
 
